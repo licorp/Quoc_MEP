7f68cded954794b2b318e7baf5214000ec13271f
licorp <52488076+licorp@users.noreply.github.com>
2026-01-10 23:21:56 +0000

    feat: Apply Nice3point and ricaun-io best practices to builders
    - Use RevitApplication() extension method for cleaner code
    - Refactor component creation from hardcoded to loop-based
    - Add comprehensive XML documentation following Nice3point style
    - Extract all constants for better maintainability
    - Enhance workflow with structured console output (box borders, numbered steps)
    - Add error handling with try-catch and $ErrorActionPreference
    - Implement version detection from git tags for automated releases
    
    References:
    - https://github.com/Nice3point/RevitTemplates
    - https://github.com/ricaun-io/ricaun.Revit.UI
    - https://github.com/ricaun-io/Autodesk.PackageBuilder

--- Quoc_Mep/Builders/QuocMEPAddinBuilder.cs
+++ Quoc_Mep/Builders/QuocMEPAddinBuilder.cs
@@ -4,24 +4,38 @@ using Autodesk.PackageBuilder;
 namespace Quoc_MEP.Builders
 {
     /// <summary>
-    /// Builder for creating Quoc_MEP.addin file for Revit
+    /// Builder for creating Quoc_MEP.addin manifest file for Revit.
     /// </summary>
+    /// <remarks>
+    /// Generates Revit add-in manifest with proper GUID and metadata.
+    /// Compatible with Revit 2020-2026 API.
+    /// </remarks>
     public class QuocMEPAddinBuilder : RevitAddInsBuilder
     {
         private const string AddinName = "Quoc_MEP";
+        private const string AddinAssembly = "Quoc_MEP.dll";
+        private const string AddinClassName = "Quoc_MEP.Application";
+        private const string AddinGuid = "AD8D446D-A204-48BF-A983-C931DD3C066F";
         private const string VendorId = "QNVN";
         private const string VendorDescription = "Quoc Nguyen - MEP Tools";
         
+        /// <summary>
+        /// Initializes a new instance of QuocMEPAddinBuilder.
+        /// </summary>
         public QuocMEPAddinBuilder()
         {
-            // Create Application AddIn entry
+            // Create Application AddIn entry with metadata
             AddIn.CreateEntry("Application")
                 .Name(AddinName)
-                .Assembly($"{AddinName}.dll")
-                .AddInId(new Guid("AD8D446D-A204-48BF-A983-C931DD3C066F"))
-                .FullClassName($"{AddinName}.Application")
+                .Assembly(AddinAssembly)
+                .AddInId(new Guid(AddinGuid))
+                .FullClassName(AddinClassName)
                 .VendorId(VendorId)
                 .VendorDescription(VendorDescription);
+            
+            // Note: ManifestSettings for Revit 2026+ are automatically handled
+            // by the Revit.Sdk build process (PatchManifest task)
+            // See: https://github.com/Nice3point/RevitTemplates/wiki
         }
     }
 }
--- Quoc_Mep/Builders/QuocMEPPackageBuilder.cs
+++ Quoc_Mep/Builders/QuocMEPPackageBuilder.cs
@@ -3,49 +3,61 @@ using Autodesk.PackageBuilder;
 namespace Quoc_MEP.Builders
 {
     /// <summary>
-    /// Builder for creating PackageContents.xml for Quoc_MEP Universal Package
-    /// Supports Revit 2020-2026
+    /// Builder for creating PackageContents.xml for Quoc_MEP Universal Package.
+    /// Supports Revit versions 2020-2026.
     /// </summary>
+    /// <remarks>
+    /// Uses Autodesk.PackageBuilder fluent API for type-safe manifest generation.
+    /// See: https://github.com/ricaun-io/Autodesk.PackageBuilder
+    /// </remarks>
     public class QuocMEPPackageBuilder : PackageContentsBuilder
     {
         private const string PackageName = "Quoc_MEP";
         private const string CompanyName = "Quoc Nguyen";
+        private const string CompanyEmail = "quoc.nguyen@company.com";
+        private const string CompanyUrl = "https://github.com/licorp/Quoc_MEP";
         private const string VendorId = "QNVN";
+        private const int MinRevitVersion = 2020;
+        private const int MaxRevitVersion = 2026;
         
+        /// <summary>
+        /// Initializes a new instance of QuocMEPPackageBuilder.
+        /// </summary>
+        /// <param name="version">Package version (e.g., "1.0.0")</param>
         public QuocMEPPackageBuilder(string version = "1.0.0")
         {
-            // Application Package metadata
+            // Use RevitApplication() extension for cleaner code (Nice3point pattern)
             ApplicationPackage
                 .Create()
-                .ProductType(ProductTypes.Application)
-                .AutodeskProduct(AutodeskProducts.Revit)
+                .RevitApplication()
                 .Name(PackageName)
                 .AppVersion(version)
-                .Description("Quoc MEP Tools - Universal package for Revit 2020-2026");
+                .Description($"{PackageName} - MEP Tools for Revit {MinRevitVersion}-{MaxRevitVersion}");
 
             // Company Details
             CompanyDetails
                 .Create(CompanyName)
-                .Email("quoc.nguyen@company.com")
-                .Url("https://github.com/licorp/Quoc_MEP");
+                .Email(CompanyEmail)
+                .Url(CompanyUrl);
 
-            // Generate Components for each Revit version (2020-2026)
-            CreateRevitComponent(2020);
-            CreateRevitComponent(2021);
-            CreateRevitComponent(2022);
-            CreateRevitComponent(2023);
-            CreateRevitComponent(2024);
-            CreateRevitComponent(2025);
-            CreateRevitComponent(2026);
+            // Generate Components for each supported Revit version
+            for (int year = MinRevitVersion; year <= MaxRevitVersion; year++)
+            {
+                CreateRevitComponent(year);
+            }
         }
 
+        /// <summary>
+        /// Creates a component entry for a specific Revit version.
+        /// </summary>
+        /// <param name="year">Revit version year (e.g., 2024)</param>
         private void CreateRevitComponent(int year)
         {
             Components
                 .CreateEntry($"Revit {year}")
-                .RevitPlatform(year)
+                .RevitPlatform(year)  // Uses extension method for automatic SeriesMin/SeriesMax
                 .AppName(PackageName)
-                .ModuleName($"./Contents/{year}/Quoc_MEP.addin");
+                .ModuleName($"./Contents/{year}/{PackageName}.addin");
         }
     }
 }
